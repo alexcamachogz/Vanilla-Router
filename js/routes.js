48a11e83d8ce5f11ed31a16ba091e6eca83dccd7
Alejandra Camacho <camacho.ale94@gmail.com>
2020-08-17 12:53:02 -0500

    Create app routes

---
+++ js/routes.js
@@ -0,0 +1,26 @@
+const PATHS = {
+    home: {
+        path: "/",
+        template: `
+            <h1>🏠 Home</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum harum aliquam reiciendis dignissimos? Perferendis consequuntur vitae fugiat fuga neque ipsum?</p>
+            <img src="https://source.unsplash.com/random/400x300" alt="Random Image">
+        `,
+    },
+    about: {
+        path: "/about",
+        template: `
+            <h1>👩🏻‍💻 Sobre mi</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum harum aliquam reiciendis dignissimos? Perferendis consequuntur vitae fugiat fuga neque ipsum?</p>
+            <img src="https://source.unsplash.com/random/500x400" alt="Random Image">
+        `,
+    },
+    contact: {
+        path: "/contact",
+        template: `
+            <h1>📱 Contacto</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum harum aliquam reiciendis dignissimos? Perferendis consequuntur vitae fugiat fuga neque ipsum?</p>
+            <img src="https://source.unsplash.com/random/600x500" alt="Random Image">
+        `,
+    }
+}
